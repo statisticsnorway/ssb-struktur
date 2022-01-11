9da115d11e3ceab412cc9abfc19af2bea7201e52
Jentoft <coo@ssb.no>
2022-01-11 13:25:59 +0100

    Updated test data to include male/female employees and male/female sick days

--- data-raw/create_data.R
+++ data-raw/create_data.R
@@ -7,6 +7,10 @@ pop_data <- data.frame(id = 1:10000,
                   employees = abs(round(rnorm(10000, mean = 20, sd = 100)))
             )
 pop_data$employees[pop_data$employees == 0] <- 2
+rand <- runif(10000)
+pop_data$employees_f <- pop_data$employees - round(rand * pop_data$employees)
+pop_data$employees_m <- pop_data$employees - pop_data$employees_f
+
 pop_data$turnover <- abs(pop_data$employees * rnorm(10000, mean = 1000, sd = 1000))
 pop_data$size <- "small"
 pop_data$size[pop_data$employees > 10] <- "mid"
@@ -14,8 +18,8 @@ pop_data$size[pop_data$employees > 100] <- "big"
 pop_data$industry <- rep(c("B", "C", "D", "E", "F"), each = 2000)
 
 # create some problems for testing
-pop_data$employees[1] <- 0
-pop_data$employees[2] <- NA
+pop_data[1, c("employees", "employees_f", "employees_m")] <- 0
+pop_data[2, c("employees", "employees_f", "employees_m")] <- NA
 
 save(pop_data, version=2, file = "data/pop_data.RData")
 
@@ -30,6 +34,10 @@ sample_data$employees[sample_data$employees == 0] <- 2
 # Add in job_vacancies
 sample_data$job_vacancies <- round(sample_data$employees * runif(1000, max = 0.3))
 sample_data$sick_days <- round(sample_data$employees * runif(1000, max = 5))
+rand2 <- runif(1000)
+sample_data$sick_days_f <- sample_data$sick_days - round(rand2 * sample_data$sick_days)
+sample_data$sick_days_m <- sample_data$sick_days - sample_data$sick_days_f
+
 
 # Add some NA values for testing
 sample_data$job_vacancies[1:10] <- NA
