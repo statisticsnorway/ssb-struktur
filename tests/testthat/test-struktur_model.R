eedae35f5c46ee2c96545fcea45f31f45f1f8b2b
Jentoft <coo@ssb.no>
2022-04-25 15:23:06 +0200

    Updated tests

--- tests/testthat/test-struktur_model.R
+++ tests/testthat/test-struktur_model.R
@@ -3,6 +3,7 @@
 
 test_that("struktur_model returns message for missing y values", {
   data(pop_data)
+  data(sample_data)
   expect_message(struktur_model(pop_data[, ], sample_data,
                                 x = "employees", y = "job_vacancies",
                                 id = "id",
@@ -12,6 +13,7 @@ test_that("struktur_model returns message for missing y values", {
 
 test_that("struktur_model stops when x values are missing", {
   data(pop_data)
+  data(sample_data)
   pop_data$employees[2] <- NA
   expect_error(struktur_model(pop_data, sample_data,
                                   x = "employees", y = "job_vacancies",
@@ -22,14 +24,15 @@ test_that("struktur_model stops when x values are missing", {
 
 test_that("struktur_model returns correct output size and beta estimate", {
   data(pop_data)
+  data(sample_data)
   suppressMessages(
-    test_results <- struktur_model(pop_data[, ], sample_data,
+    test_results <- struktur_model(pop_data, sample_data,
                                  x = "employees", y = "job_vacancies",
                                  id = "id",
                                  strata = "industry")
   )
   expect_equal(nrow(test_results), nrow(pop_data[, ]))
-  expect_equal(test_results$job_vacancies_beta[1], 0.145902357)
+  expect_equal(as.numeric(test_results$job_vacancies_beta[1]), 0.145902357)
 })
 
 
